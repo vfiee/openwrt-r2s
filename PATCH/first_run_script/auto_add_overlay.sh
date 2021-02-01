94e10a412c58715633a850432575de15c52a4ec9
Miniers <m@lk.mk>
2021-02-01 13:06:53 +0800

    add auto reboot

--- PATCH/first_run_script/auto_add_overlay.sh
+++ PATCH/first_run_script/auto_add_overlay.sh
@@ -9,3 +9,4 @@ w
 " | fdisk /dev/mmcblk0 && mkfs.ext4 /dev/mmcblk0p3
 block detect | sed 's/\/overlay/\/mnt\/loop0/g' | sed '$d' | sed '$s/0/1/' | sed 's/mnt\/mmcblk0p3/overlay/' | uci import fstab
 
+sleep 5 && touch /etc/banner && reboot
